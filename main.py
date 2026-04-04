24ffe9c144a203348ce083aead2edb26d6b71707
Ilariy <147840605+yuIlariy@users.noreply.github.com>
2026-04-05 00:19:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
-import logging
 import asyncio
 from pathlib import Path
 from dotenv import load_dotenv
 
+from src.bot import ZenloadBot
+from src.utils.pyro_client import app as pyro_app
+
+
+async def start_pyro():
+    await pyro_app.start()
+    print("🚀 Pyrogram started")
+
+
 if __name__ == "__main__":
     load_dotenv(Path(__file__).parent / '.env')
 
-    from src.bot import ZenloadBot
-    from src.utils.pyro_client import app as pyro_app
+    print("🟢 Bot is starting...")
 
-    print("🟢 Bot is attempting to start...")
+    # Start Pyrogram in background loop
+    loop = asyncio.get_event_loop()
+    loop.create_task(start_pyro())
 
-    async def start_pyro():
-        await pyro_app.start()
-        print("🚀 Pyrogram started")
-
-    asyncio.run(start_pyro())
-
-    # Run normal bot
     bot = ZenloadBot()
     bot.run()
